0bd7bb3b2ca5ddcb287e84bbc99514a70554e62d
Ryan Durrant <rddurrant@gmail.com>
2017-07-29 15:30:52 -0700

    small changes

--- jquery.js
+++ jquery.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 
-  $("h1.fadeIn").fadeIn(3000).removeClass('hidden');
+  $("h1.fadeIn").fadeIn(3000);
+
+$(function(){
+  window.setTimeout(fade2, 2000);
+
+  function fade2(){
+    $("h1.fadeIn2").fadeIn(3000);
+  }
+});
+  // $("h1.fadeIn3").fadeIn(9000);
+
 
 });
